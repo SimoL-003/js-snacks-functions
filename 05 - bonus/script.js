3cafec91bbf78d213db9646308e252856a8695a5
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-09-27 09:01:06 +0200

    exercise 05

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -6,13 +6,36 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+const date = new Date();
+let hour = date.getHours();
 
 // Dichiara la funzione qui.
 
+/**
+ * 
+ * @param {string} name 
+ * @returns {string}
+ * 
+ * Se l'ora è minore o uguale a 13, allora dici Buongiorno; se altrimenti l'ora è compresa tra 14 e 17, allora dici Buon pomeriggio; altrimenti dici Buonasera
+ */
+
+function sayCiao(name) {
+    let message = "";
+    if (hour <= 13) {
+        message = "Buongiorno ";
+    } else if (hour > 13 && hour <= 17) {
+        message = "Buon pomeriggio ";
+    } else {
+        message = "Buonasera ";
+    }
+
+    message = message + name
+    return message
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(sayCiao(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
